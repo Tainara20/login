5baa96fe3a2032d4b3ea31be6c8dd3dc1a9751e7
Tainara20 <Tainara_Santos5@estudante.sc.senai.br>
2019-09-30 19:58:10 -0300

    inicío de registro de formulário
    inicío de registro de formulário

--- index.php
+++ index.php
@@ -112,6 +112,46 @@
             </div>
         </section>
         <!-- Fim da sação de recuperação de senha -->
+
+        <!-- Inicío do formulário de cadastro de novos usuários -->
+
+        <section class="row mt-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+                <h2 class="text-center mt-2">Registre-se aqui</h2>
+                <form action="#" method="post" class="p-2" id="formRegistro" placeholder="Nome completo">
+
+                    <div class="form-group">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de Usuáro">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha Usuário">
+
+                    </div>
+
+
+            </div>
+
+            </form>
+
+
+
+
+            </div>
+
+        </section>
+
+        <!-- Final do formulário de cadastro de novos usuários -->
+
     </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
